2f0593dd124e0a76e69fe215792aa99ca6be3699
Usha <ushamatsc21@pgdav.du.ac.in>
2025-01-31 08:30:12 +0000

    Created landing page, added lenis for smooth scroll and added gsap for animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { WorkoutFormComponent } from './workout-form/workout-form.component';
 import { WorkoutListComponent } from './workout-list/workout-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import Lenis from '@studio-freight/lenis';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WorkoutFormComponent, WorkoutListComponent],
+  imports: [RouterOutlet, LandingPageComponent, WorkoutFormComponent, WorkoutListComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'workout-tracker';
+  lenis = new Lenis();
+  
+  ngAfterViewInit() {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    });
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { WorkoutFormComponent } from './workout-form/workout-form.component';
+import { WorkoutListComponent } from './workout-list/workout-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'workout-form', component: WorkoutFormComponent },
+  { path: 'workout-list', component: WorkoutListComponent },
+];
