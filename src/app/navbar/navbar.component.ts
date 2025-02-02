7148015f1973126342e553963a613f1fcafba4cc
Usha <ushamatsc21@pgdav.du.ac.in>
2025-02-02 19:47:26 +0530

    Update navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent { }
+export class NavbarComponent { 
+  showMenu = false;
+
+  toggleMobileMenu() {
+    this.showMenu = !this.showMenu;
+    console.log('showMenu', this.showMenu);
+  }
+}
